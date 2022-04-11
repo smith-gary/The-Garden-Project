6c9a8cf2fea188ce70fcf14ff2929848a98ff17b
gary_s <smithgd34@gmail.com>
2022-04-11 15:28:00 -0500

    Project structure

---
+++ src/main/java/com/tgp/tgp/TgpApplication.java
@@ -0,0 +1,13 @@
+package com.tgp.tgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgp/tgp/TgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgp.tgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
